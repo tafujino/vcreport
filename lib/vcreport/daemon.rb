5db2cfbbd28de3e655fb4f100412ca8df5b7210f
Takeshi Fujino <fujino@edu.k.u-tokyo.ac.jp>
2020-06-05 18:17:51 +0900

    FIX: fix a bug

--- lib/vcreport/daemon.rb
+++ lib/vcreport/daemon.rb
@@ -13,11 +13,8 @@ module VCReport
       # @param data_dir [String]
       # @param report_dir [String]
       def run(data_dir, report_dir)
-        sample_dir = Pathname.new(sample)
-
         loop do
-          reports = scan_samples(data_dir).map do |sample|
-            sample_dir = data_dir / sample
+          reports = scan_samples(data_dir).map do |sample_dir|
             report = sample_report(sample_dir)
             report.generate_html(report_dir)
             report
@@ -32,7 +29,7 @@ module VCReport
       # @param data_dir [Pathname]
       # @return         [Array<Pathname>]
       def scan_samples(data_dir)
-        DIr[data_dir / '*']
+        Dir[data_dir / '*']
           .map { |e| Pathname.new(e) }
           .select(&:directory?)
       end
