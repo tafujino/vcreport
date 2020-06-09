ffcd0f2fa20678ac2d0df8b8b39eb3c2e8f842c5
Takeshi Fujino <fujino@edu.k.u-tokyo.ac.jp>
2020-06-09 18:27:09 +0900

    FEAT: update start command

--- lib/vcreport.rb
+++ lib/vcreport.rb
@@ -18,29 +18,12 @@ module VCReport
         say 'Start a report daemon'
         say "Data directory: #{data_dir}"
         say "Report directory: #{report_dir}"
-        ret = system "#{vcrepd_path} #{data_dir} #{report_dir}"
-        unless ret
-          warn 'Report dameon failed'
-          exit 1
-        end
+        pid = spawn "#{vcrepd_path} #{data_dir} #{report_dir} 2>1 > /dev/null"
+        Process.detach(pid)
       end
 
       desc 'list', 'list running daemons'
       def list
-        processes = `ps -ef`.split("\n").map do |line|
-          fields = line.chomp.split(/\s+/)
-          pid = fields[1]
-          cmd = fields[8..-1]
-          pp cmd
-          next nil unless cmd[0] =~ %r{(^|/)ruby$} && cmd[1] =~ %r{(^|/)vcrepd$}
-
-          [pid, cmd[2], cmd[3]]
-        end.compact
-        if processes.empty?
-          warn '(empty)'
-          return
-        end
-
         warn %w[PID DATA_DIR REPORT_DIR].join("\t")
         processes.each do |pid, data_dir, report_dir|
           warn [pid, data_dir, report_dir].join("\t")
