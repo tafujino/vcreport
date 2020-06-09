de0b74f376b378c16b885b610ab8588416c39fbd
Takeshi Fujino <fujino@edu.k.u-tokyo.ac.jp>
2020-06-09 15:56:55 +0900

    FIX: fix a bug

--- lib/vcreport.rb
+++ lib/vcreport.rb
@@ -4,13 +4,10 @@ require 'vcreport/version'
 require 'vcreport/daemon'
 require 'vcreport/generate'
 require 'thor'
-require 'rake'
 
 module VCReport
   module CLI
     class Main < Thor
-      extend Rake::DSL
-
       def self.exit_on_failure?
         true
       end
@@ -18,7 +15,10 @@ module VCReport
       desc 'start [DATA_DIR] [REPORT_DIR]', 'Start reporting'
       def start(data_dir, report_dir)
         vcrepd_path = File.expand_path('vcrepd', __dir__)
-        sh "#{vcrepd_path} #{data_dir} #{report_dir}"
+        say 'Start a report daemon'
+        say "data directory: #{data_dir}"
+        say "report directory: #{report_dir}"
+        system "#{vcrepd_path} #{data_dir} #{report_dir}"
       end
 
       desc 'stop [DATA_DIR]', 'Stop reporting'
